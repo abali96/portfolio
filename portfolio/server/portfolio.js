7854874ba5956cd1d335d77bf372a67addae343e
Arjun Bali <arjun.bali96@gmail.com>
2017-01-24 01:04:10 -0500

    Add LinkedIn and hide overflow on webdevgall captions

--- portfolio/server/portfolio.js
+++ portfolio/server/portfolio.js
@@ -4,15 +4,18 @@ Meteor.startup(function() {
   Organizations.remove({});
   Categories.insert({name: "robotics-wrapper", projects: [Meteor.mechatronics.jeeves, Meteor.mechatronics.matrix, Meteor.mechatronics.breathaliver, Meteor.mechatronics.adelaide, Meteor.mechatronics.frame_me]});
   Categories.insert({name: "webdev-wrapper", projects: [Meteor.webdev.replay, Meteor.webdev.freeloader]});
-  Meteor.organizations.icspi["key"] = 1;
-  Meteor.organizations.voltera["key"] = 2;
-  Meteor.organizations.bitmaker["key"] = 3;
-  Meteor.organizations.deca["key"] = 4;
-  Meteor.organizations.glowstik["key"] = 5;
-  Meteor.organizations.top_scholar["key"] = 6;
-  Meteor.organizations.ccs["key"] = 7;
+
+  Meteor.organizations.linkedin["key"] = 1;
+  Meteor.organizations.icspi["key"] = 2;
+  Meteor.organizations.voltera["key"] = 3;
+  Meteor.organizations.bitmaker["key"] = 4;
+  Meteor.organizations.deca["key"] = 5;
+  Meteor.organizations.glowstik["key"] = 6;
+  Meteor.organizations.top_scholar["key"] = 7;
+  // Meteor.organizations.ccs["key"] = 7;
   Meteor.organizations.phasma["key"] = 8;
 
+  Organizations.insert(Meteor.organizations.linkedin);
   Organizations.insert(Meteor.organizations.icspi);
   Organizations.insert(Meteor.organizations.voltera);
   Organizations.insert(Meteor.organizations.bitmaker);
@@ -20,6 +23,6 @@ Meteor.startup(function() {
   Organizations.insert(Meteor.organizations.glowstik);
   // Organizations.insert(Meteor.organizations.mytf);
   Organizations.insert(Meteor.organizations.top_scholar);
-  Organizations.insert(Meteor.organizations.ccs);
+  // Organizations.insert(Meteor.organizations.ccs);
   Organizations.insert(Meteor.organizations.phasma);
 });
